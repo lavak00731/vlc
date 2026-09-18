e91fc19bfafdcc0307a35753b21cf014a125bf7f
lavak00731 <eolavaca@gmail.com>
2026-09-18 14:23:22 -0300

    adding id to mobile menu

--- src/app/components/navs/MobileNav.tsx
+++ src/app/components/navs/MobileNav.tsx
@@ -1,23 +1,25 @@
 "use client";
 import Link from "next/link";
-import { useState } from "react";
+import { useState, useId } from "react";
 import NavItems from "@/app/utils/NavItems";
 import { IsNavigationActive } from "@/app/utils/isNavigationActive";
 
 
+
 export const MobileNav = () => {
+    const id = useId();
     const [btnStatus, setBtnStatus] = useState<boolean>(false);
     const handleClick = () => {
         setBtnStatus(!btnStatus)
     }
   return (
     <>
-        <button onClick={handleClick} type="button" aria-expanded={btnStatus} className="p-1 flex flex-col align-middle bg-transparent rounded-sm border-outline md:hidden">
+        <button onClick={handleClick} type="button" aria-expanded={btnStatus} aria-controls={id} className="p-1 flex flex-col align-middle bg-transparent rounded-sm border-outline md:hidden">
             <span aria-hidden="true" className="material-symbols-outlined text-primary text-[32px]" hidden={btnStatus ? true : false}>menu</span>
             <span aria-hidden="true" className="material-symbols-outlined text-primary text-[32px] transition-colors" hidden={btnStatus ? false : true}>close</span>
             <span className="sr-only">Menu</span>
         </button>
-        <nav className="absolute top-[64px] left-0 items-center gap-space-lg shadow-elevated w-full bg-vivero-badge-stock md:hidden" hidden={btnStatus ? false : true}>
+        <nav id={id} className="absolute top-16 left-0 items-center gap-space-lg shadow-elevated w-full bg-vivero-badge-stock md:hidden" hidden={btnStatus ? false : true}>
             <ul className="flex flex-col list-none gap-5 p-4">
                 {NavItems.map((item) => {
                 const active = IsNavigationActive(item.url);
